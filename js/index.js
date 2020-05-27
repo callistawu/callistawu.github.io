a7008e0c22c516ce73fd25a804ff9f15b4e65ab7
Ryan Yang <ryanqyang@gmail.com>
2020-05-26 21:03:43 -0700

    set site to display resume

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const pdfjsLib = require('pdfjs-dist');
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+'../../node_modules/pdfjs-dist/build/pdf.worker.js';
+const url = "../assets/resume.pdf";
+
+let loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(function (pdf) {
+    pdf.getPage(1).then(function (page) {
+        let scale = 2;
+        let viewport = page.getViewport({ scale: scale, });
+        let canvas = document.getElementById('resume');
+        let context = canvas.getContext('2d');
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        let renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+        };
+        page.render(renderContext);
+    });
+})
+document.addEventListener('webviewerloaded', function() {
+    PDFViewerApplicationOptions.set('printResolution', 60);
+  });
