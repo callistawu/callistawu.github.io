03ae6432b895cf4d775b23974f53eb8a3ebf3b07
Ryan Yang <ryanqyang@gmail.com>
2020-05-28 00:03:21 -0700

    added textlayer

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 const url = "../assets/resume.pdf";
 
-let loadingTask = pdfjsLib.getDocument(url);
-loadingTask.promise.then(function (pdf) {
-    pdf.getPage(1).then(function (page) {
-        let scale = 2;
-        let viewport = page.getViewport({ scale: scale, });
-        let canvas = document.getElementById('resume');
-        let context = canvas.getContext('2d');
+const load = () => {
+    let eventBus = new pdfjsViewer.EventBus();
 
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        let renderContext = {
-            canvasContext: context,
-            viewport: viewport,
-        };
-        page.render(renderContext);
+    let loadingTask = pdfjsLib.getDocument({
+        url: url
     });
-})
-document.addEventListener('webviewerloaded', function() {
-    PDFViewerApplicationOptions.set('printResolution', 60);
-  });
+
+    loadingTask.promise.then(function (pdfDocument) {
+        return pdfDocument.getPage(1).then(function (pdfPage) {
+            let pdfPageView = new pdfjsViewer.PDFPageView({
+                container: document.getElementById("resume"),
+                id: 1,
+                scale: 1.1,
+                defaultViewport: pdfPage.getViewport({ scale: 1 }),
+                eventBus: eventBus,
+                textLayerFactory: new pdfjsViewer.DefaultTextLayerFactory(),
+                annotationLayerFactory: new pdfjsViewer.DefaultAnnotationLayerFactory(),
+            });
+
+            pdfPageView.setPdfPage(pdfPage);
+            return pdfPageView.draw();
+        });
+    });
+}
